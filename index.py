5b9bfe047c0d3cf05ee6361fcea325c5d6225bc3
deepakbi <55545369+deepakbi@users.noreply.github.com>
2019-10-22 10:21:14 +0530

    Update index.py

--- index.py
+++ index.py
@@ -7,6 +7,6 @@ def handler(event, context):
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
-    return {'statusCode': 2000,
+    return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
